45333f50425546b808c13bf023f15ed1e13fb375
th3corinthian <sam.feldman724@proton.me>
2025-11-18 20:33:03 -0500

    android r/e tools

--- modules/virt/android.nix
+++ modules/virt/android.nix
@@ -7,10 +7,13 @@
     description = "enable android emulator and SDK tools";
   };
 
-  config = lib.mkIf config.androidUtilsUtils.enable {
+  config = lib.mkIf config.androidUtils.enable {
     environment.systemPackages = with pkgs; [
       android-tools
       android-studio
+      genymotion
+      dex2jar
+      frida-tools
       #cabextract                # winetricks helper
       # optional helpers:
       #lutris                    # if you like GUIs for Wine
