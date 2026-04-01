1596165d580930b17cb1f6b3f7cd5107816398a0
Sam Feldman <sam.feldman724@proton.me>
2026-04-01 18:22:48 -0400

    edited fail2ban

--- system/configuration.nix
+++ system/configuration.nix
@@ -128,13 +128,13 @@ in
        # Ban IP after 5 failures
        maxretry = 5;
        bantime = "24h"; # Ban IPs for one day on the first ban
-       bantime-increment = {
-         enable = true; # Enable increment of bantime after each violation
-         formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
-         multipliers = "1 2 4 8 16 32 64";
-         maxtime = "168h"; # Do not ban for more than 1 week
-         overalljails = true; # Calculate the bantime based on all the violations
-       };
+       #bantime-increment = {
+         #enable = true; # Enable increment of bantime after each violation
+         #formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+         #multipliers = "1 2 4 8 16 32 64";
+         #maxtime = "168h"; # Do not ban for more than 1 week
+         #overalljails = true; # Calculate the bantime based on all the violations
+       #};
        jails = {
          apache-nohome-iptables.settings = {
            # Block an IP address if it accesses a non-existent
