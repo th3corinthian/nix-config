49758810453d0981a86e59928137b77d2ba98f7b
Sam Feldman <sam.feldman724@proton.me>
2026-04-01 18:28:11 -0400

    edited fail2ban

--- system/configuration.nix
+++ system/configuration.nix
@@ -123,11 +123,11 @@ in
       allowSFTP = true;
     };
 
-     fail2ban = {
-       enable = true;
+     #fail2ban = {
+       #enable = true;
        # Ban IP after 5 failures
-       maxretry = 5;
-       bantime = "24h"; # Ban IPs for one day on the first ban
+       #maxretry = 5;
+       #bantime = "24h"; # Ban IPs for one day on the first ban
        #bantime-increment = {
          #enable = true; # Enable increment of bantime after each violation
          #formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
@@ -135,21 +135,21 @@ in
          #maxtime = "168h"; # Do not ban for more than 1 week
          #overalljails = true; # Calculate the bantime based on all the violations
        #};
-       jails = {
-         apache-nohome-iptables.settings = {
+       #jails = {
+         #apache-nohome-iptables.settings = {
            # Block an IP address if it accesses a non-existent
            # home directory more than 5 times in 10 minutes,
            # since that indicates that it's scanning.
-           filter = "apache-nohome";
-           action = ''iptables-multiport[name=HTTP, port="http,https"]'';
-           logpath = "/var/log/httpd/error_log*";
-           backend = "auto";
-           findtime = 600;
-           bantime  = 600;
-           maxretry = 5;
-         };
-       };
-     };
+           #filter = "apache-nohome";
+           #action = ''iptables-multiport[name=HTTP, port="http,https"]'';
+           #logpath = "/var/log/httpd/error_log*";
+           #backend = "auto";
+           #findtime = 600;
+           #bantime  = 600;
+           #maxretry = 5;
+         #};
+       #};
+     #};
 
     # Yubikey smart card mode (CCID)
     #pcscd.enable = true;
